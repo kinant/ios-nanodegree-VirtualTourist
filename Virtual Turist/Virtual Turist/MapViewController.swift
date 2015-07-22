ad529908d29f95e9736a91e46f10a57bc73d0cc3
Kinan T <kinan.t@gmail.com>
2015-07-22 02:22:31 -0600

    Added long press gesture to add a pin to the map. Map zooms to pin. Pin drop is animated!

--- Virtual Turist/Virtual Turist/MapViewController.swift
+++ Virtual Turist/Virtual Turist/MapViewController.swift
@@ -18,8 +18,45 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         mapView.delegate = self
+        mapView.mapType = .Standard
+        
+        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self , action: "handleLongPress:")
+        longPressGestureRecognizer.minimumPressDuration = 1.0
+        self.mapView.addGestureRecognizer(longPressGestureRecognizer)
     }
-
+    
+    func handleLongPress(recognizer: UILongPressGestureRecognizer){
+        println("long press!")
+        if(recognizer.state == UIGestureRecognizerState.Began){
+        
+            var point = recognizer.locationInView(self.mapView)
+            var locationCoordinate = self.mapView.convertPoint(point, toCoordinateFromView: self.mapView)
+        
+            let annotation = VTAnnotation(coordinate: locationCoordinate, title: "New Location", subtitle: "New Subtitle")
+            mapView.addAnnotation(annotation)
+            setCenterOfMapToLocation(locationCoordinate)
+        }
+    }
+    
+    func setCenterOfMapToLocation(location: CLLocationCoordinate2D) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func mapView(mapView: MKMapView!, didAddAnnotationViews views: [AnyObject]!) {
+        for view in views {
+            var annotationView = view as! MKAnnotationView
+            var endFrame = annotationView.frame
+            
+            annotationView.frame = CGRectOffset(endFrame, 0, -500)
+            
+            UIView.animateWithDuration(0.5, animations: {
+                annotationView.frame = endFrame
+            })
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
