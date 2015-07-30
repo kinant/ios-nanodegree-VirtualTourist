522ce0064bda14f3870061e2d206d0e903d5966a
Kinan T <kinan.t@gmail.com>
2015-07-29 23:41:11 -0600

    Small fixes.

--- Virtual Tourist/Virtual Tourist/PinDetailViewController.swift
+++ Virtual Tourist/Virtual Tourist/PinDetailViewController.swift
@@ -69,6 +69,19 @@ class PinDetailViewController: UIViewController, UICollectionViewDelegateFlowLay
         */
     }
     
+    override func viewDidLayoutSubviews() {
+        // Lay out the collection view so that cells take up 1/3 of the width,
+        // with no space in between.
+        let layout : UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.minimumLineSpacing = 5
+        layout.minimumInteritemSpacing = 5
+        
+        let width = floor(self.collectionView!.frame.size.width/4)
+        layout.itemSize = CGSize(width: width, height: width)
+        collectionView!.collectionViewLayout = layout
+    }
+    
     // Mark: - Fetched Results Controller
     
     lazy var fetchedResultsController: NSFetchedResultsController = {
